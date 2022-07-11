47dcc5c4dfb62551d83024680086119123d0b975
Hugo Osvaldo Barrera <hugo@barrera.io>
2022-07-11 09:02:38 +0200

    Correct licence on single source file
    This file was originally extracted from libcaffeine, which was ISC
    licensed. However, as part of caffeine, it should now be distributed as
    GPLv3+. It's unclear whether the ISC license can continue to apply to
    it, and I'd rather opt for the simpler / less problematic approach.

--- caffeine/inhibitors.py
+++ caffeine/inhibitors.py
@@ -1,19 +1,7 @@
-# Copyright (c) 2015 Hugo Osvaldo Barrera <hugo@barrera.io>
+# Copyright (c) 2015-2022 Hugo Osvaldo Barrera <hugo@barrera.io>
 #
-# Permission to use, copy, modify, and distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-# Note: This code is part libcaffeine, though it's been moved in-tree (rather
-# than handled as a proper dependency) because libcaffeine isn't really stable
-# enough for public release.
+# SPDX-License-Identifier: GPL-3.0+
+
 import logging
 import os
 import threading
