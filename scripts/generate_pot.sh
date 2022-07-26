1d43d45e8c07752123fc9396f5ff08c54d168b7d
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2022-07-26 22:27:25 +0200

    Delete some more superceded scripts

--- scripts/generate_pot.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-find . -iname "*.py" -o -iname "*.glade" | \
-    xargs xgettext --from-code utf-8 -o translations/caffeine.pot
--- scripts/update_translations.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-scripts/generate_pot.sh
-scripts/compile_translations.py caffeine translations
